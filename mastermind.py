a4ff60b56220aaabc8ef7cac14418400769b2bc7
Pattapon Gowanit <iampattapon@gmail.com>
2023-11-21 19:15:40 +0700

    Finished Version

--- mastermind.py
+++ mastermind.py
@@ -5,72 +5,73 @@ class Setup:
     def __init__(self, colour_num, position_num, duplicate='no'):
         self.colour = colour_num
         self.position = position_num
-        self.answer = ''
+        self.__answer = ''
         self.duplicate = duplicate
 
     def create_answer(self):
+        temp = ''
         if self.duplicate == 'yes':
             for i in range(self.position):
                 random_num = random.randint(1, self.colour)
-                self.answer += str(random_num)
+                temp += str(random_num)
 
         elif self.duplicate == 'no':
-            temp = ''
             while len(temp) != self.position:
                 for i in range(self.position):
                     random_num = random.randint(1, self.colour)
                     if str(random_num) not in temp:
                         temp += str(random_num)
-            self.answer = temp
-            return self.answer
+            return temp
+
+    @property
+    def answer(self):
+        return self.__answer
 
 
 class Mastermind:
     def __init__(self, guess):
         self.guess = guess
-        self.hint = ''
+        self.__hint = ''
 
-    def gameplay(self):
+    def gameplay(self, answer):
         n = 0
         while self.guess != answer:
             self.guess = input('Enter your guess: ')
+            # elif self.guess == '9999':
+            #     print(answer)
             print(f'Your guess is {self.guess}')
-            self.hint = ''
+            self.__hint = ''
             for num in range(len(self.guess)):
                 if self.guess[num] == answer[num]:
-                    self.hint += '*'
-                elif self.guess[num] in answer:
-                    self.hint += 'o'
+                    self.__hint += '*'
+            for num in range(len(self.guess)):
+                if self.guess[num] in answer and self.guess[num] != answer[num]:
+                    self.__hint += 'o'
+
             print(self.hint)
             print()
             n += 1
         print(f'You solve it after {n} rounds')
         print('================================')
 
+    @property
+    def hint(self):
+        return self.__hint
+
 
 choice = ''
+Mastermind('')
 while choice != 'no':
     color_num = int(input('How many colours: '))
-    position_num = int(input('How many positions: '))
-    duplicate = input('Allow duplicates? (yes/no): ')
+    positions = int(input('How many positions: '))
+    dup = input('Allow duplicates? (yes/no): ')
 
-    game_setup = Setup(color_num, position_num, duplicate)
+    game_setup = Setup(color_num, positions, dup)
     game_setup.create_answer()
-    answer = game_setup.answer
+    ans = game_setup.answer
 
-    print(f'Playing Mastermind with {color_num} colours and {position_num} positions')
-    print(answer)
+    print(f'Playing Mastermind with {color_num} colours and {positions} positions')
     mastermind_game = Mastermind('')
-    mastermind_game.gameplay()
+    mastermind_game.gameplay(ans)
     Mastermind('')
     choice = input('Do you want to continue? ')
-
-
-
-
-
-
-
-
-
-
